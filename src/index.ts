39705b1de020a66af965289e8635e0da2b66eb10
Brayan Sanjuan <bryan.sj175@gmail.com>
2024-11-13 16:19:46 -0500

    feat: :sparkles: read inputs and show data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+import * as core from '@actions/core';
+// import * as github from '@actions/github';
+
+async function run(): Promise<void> {
+  try {
+    const level = core.getInput('level');
+    const currentVersion = core.getInput('current_version');
+
+    console.log({ currentVersion, level });
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+}
+
+run();
